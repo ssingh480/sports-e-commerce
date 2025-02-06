dfec6122a09c54c372970c199a61658f4bb0eeba
ssingh480 <ssingh480@rku.ac.in>
2025-02-06 11:37:23 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Add interactivity here (e.g., cart functionality, search, etc.)
-// Example: Add to Cart button functionality
-const addToCartButtons = document.querySelectorAll('.product-card button');
-
-addToCartButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    alert('Product added to cart!');
-  });
-});
